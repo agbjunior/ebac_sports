252e94d1604fb4160d775ca416bdfb6916dafe50
agbjunior <adilsonguimaraes1@hotmail.com>
2024-04-22 13:56:16 +0100

    commit inicial

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -2,6 +2,7 @@ import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
 
 import * as S from './styles'
+import { useGetProdutoQuery } from '../services/api'
 
 type Props = {
   produtos: ProdutoType[]
@@ -10,12 +11,11 @@ type Props = {
   favoritar: (produto: ProdutoType) => void
 }
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+const ProdutosComponent = ({ favoritos, favoritar }: Props) => {
+  const { data: produtos, isLoading } = useGetProdutoQuery()
+
+  if (isLoading) return <h2>Carregando...</h2>
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
     const IdsDosFavoritos = favoritos.map((f) => f.id)
@@ -26,13 +26,12 @@ const ProdutosComponent = ({
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
             favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
